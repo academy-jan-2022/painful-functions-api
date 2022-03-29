1ff0cfb5f26a2c3881baf78542208987a11bf1f1
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-03-29 10:17:48 +0200

    Some cleanup

--- Codurance.FunctionAPI/GenerateGUID.cs
+++ Codurance.FunctionAPI/GenerateGUID.cs
@@ -1,45 +1,41 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Azure.Storage.Blobs;
-using Azure.Storage.Blobs.Models;
 
+namespace Codurance.FunctionAPI;
 
-namespace Codurance.FunctionAPI
+public static class GenerateGUID
 {
-    public static class GenerateGUID
+    private const string DivisibleByFourTemplate = "The number {0} is divisible by 4. Woo!";
+
+    [FunctionName("GenerateGUID")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]
+        HttpRequest req,
+        [Queue("api")] [StorageAccount("AzureWebJobsStorage")]
+        ICollector<string> msg,
+        ILogger log)
     {
-        [FunctionName("GenerateGUID")]
-        
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
-            [Queue("api"),StorageAccount("AzureWebJobsStorage")] ICollector<string> msg,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+        log.LogInformation("C# HTTP trigger function processed a request");
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            NumberRequest data = JsonConvert.DeserializeObject<NumberRequest>(requestBody); 
-            
-            Guid guid = Guid.NewGuid();
+        var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        var data = JsonConvert.DeserializeObject<NumberRequest>(requestBody);
 
-            if (data.number % 4 == 0)
-            {
-                log.LogInformation($"The number {data.number} is divisible by 4. Woo!");
-            }
-            
-            NumberMessage message = new NumberMessage(data.number, guid);
-           
-            msg.Add(JsonConvert.SerializeObject(message));
+        var guid = Guid.NewGuid();
 
-            return new OkObjectResult(guid);
-        }
+        if (data.number % 4 == 0)
+            log.LogInformation(DivisibleByFourTemplate, data.number);
 
+        var message = new NumberMessage(data.number, guid);
+
+        msg.Add(JsonConvert.SerializeObject(message));
+
+        return new OkObjectResult(guid);
     }
 }
--- Codurance.FunctionAPI/NumberMessage.cs
+++ Codurance.FunctionAPI/NumberMessage.cs
@@ -1,16 +1,15 @@
-namespace Codurance.FunctionAPI;
-
 using System;
+
+namespace Codurance.FunctionAPI;
 
 public struct NumberMessage
 {
     public int Number;
     public Guid GUID;
 
-    public NumberMessage(int number, Guid GUID) {
-        this.Number = number;
-        this.GUID = GUID;
-
+    public NumberMessage(int number, Guid guid)
+    {
+        Number = number;
+        GUID = guid;
     }
-
-} 
+}
